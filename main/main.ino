eb02c8eaa357e02f1cb771145d051513cfbec3bd
Tran Phuoc Loc <giongnhuntlf@gmail.com>
2021-04-23 22:26:27 +0700

    init project with LCD-I2C hello world

---
+++ main/main.ino
@@ -0,0 +1,18 @@
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x27,16,2);  // set the LCD address to 0x27 for a 16 chars and 2 line display
+// 0x27, 03F
+void setup()
+{
+  lcd.init();                      // initialize the lcd 
+  // Print a message to the LCD.
+  lcd.backlight();
+  lcd.setCursor(3,0);
+  lcd.print("Duoc chua ban oi
+  ");
+}
+
+
+void loop() {
+  
+}
